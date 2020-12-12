12db3e2656e4720f64c26bfd423d0011a209bab5
DeveloperDreamming <developerdreamming@gmail.com>
2020-12-12 13:30:34 +0900

    GPIO Input Test COde

---
+++ GPIO_Input/InputCheck.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+
+import time
+
+
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(16,GPIO.OUT)
+
+GPIO.setup(20,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+
+#GPIO.setup(20,GPIO.IN)
+
+try:
+  while 1:
+    if GPIO.input(20)==1:
+      GPIO.output(16,0)
+    else:
+      GPIO.output(16,1)
+ 
+finally:
+  GPIO.cleanup()
