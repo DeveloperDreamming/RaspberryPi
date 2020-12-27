f54837f7d2c1e91b5313a277401143832b062131
Developerdreamming <developerdreamming@gmail.com>
2020-12-27 23:28:10 +0900

    PIR Sensor Input Prg(Python) Upload

---
+++ GPIO_PIR/PIRInput.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(16,GPIO.OUT)
+GPIO.setup(21,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+#GPIO.setup(21,GPIO.IN)
+
+def switchPressed(channel):
+  print("Someone is Detected")
+  GPIO.output(16,1)
+  time.sleep(1)
+  GPIO.output(16,0)
+
+
+GPIO.add_event_detect(21, GPIO.RISING, callback=switchPressed)
+
+try:
+  while 1:
+    print(".")
+    time.sleep(5)
+finally:
+  GPIO.cleanup()
